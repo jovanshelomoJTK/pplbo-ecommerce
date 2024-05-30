12862157a3fdf66c6c38f209bbe531acf2ca0134
Rivaldo Fauzan <rivaldofr03@gmail.com>
2024-05-30 13:00:43 +0700

    update sql, model, repo

--- database/migrations/cart/V2__Create_table_cart.sql
+++ database/migrations/cart/V2__Create_table_cart.sql
@@ -8,6 +8,7 @@ CREATE TABLE IF NOT EXISTS Cart (
 );
 
 -- Insert some initial data into the Cart table
-INSERT INTO Cart (product_id, user_id, quantity, price) VALUES ('Product-1', "User-1", 2, 100.0);
-INSERT INTO Cart (product_id, user_id, quantity, price) VALUES ('Product-2', "User-2", 1, 200.0);
-INSERT INTO Cart (product_id, user_id, quantity, price) VALUES ('Product-3', "User-3", 3, 50.0);
+INSERT INTO Cart (product_id, user_id, quantity, price) VALUES ('Product-1', 'User-1', 2, 100.0);
+INSERT INTO Cart (product_id, user_id, quantity, price) VALUES ('Product-2', 'User-2', 1, 200.0);
+INSERT INTO Cart (product_id, user_id, quantity, price) VALUES ('Product-3', 'User-3', 3, 50.0);
+
