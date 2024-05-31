4d91f25dc948b4e9502298c72d88ae74e6e9d065
sendist <sendisetiawan10902@gmail.com>
2024-05-31 17:12:54 +0700

    add event with kafka

--- order-service/src/main/java/com/pplbo/orderservice/controller/OrderController.java
+++ order-service/src/main/java/com/pplbo/orderservice/controller/OrderController.java
@@ -4,10 +4,11 @@ import com.pplbo.orderservice.model.Order;
 import com.pplbo.orderservice.service.OrderService;
 
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
@@ -20,5 +21,10 @@ public class OrderController {
     @GetMapping("/orders")
     public List<Order> getAllOrder() {
         return orderService.getAllOrder();
+    }
+
+    @PostMapping("/order")
+    public Order createOrder(@RequestBody Order order) {
+        return orderService.createOrder(order);
     }
 }
--- order-service/src/main/java/com/pplbo/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/pplbo/orderservice/service/OrderService.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.pplbo.orderservice.event.OrderCreatedEvent;
+import com.pplbo.orderservice.kafka.OrderProducer;
 import com.pplbo.orderservice.model.Order;
 import com.pplbo.orderservice.repository.OrderRepository;
 
@@ -14,7 +16,18 @@ public class OrderService {
     @Autowired
     private OrderRepository orderRepository;
 
+    @Autowired
+    private OrderProducer orderProducer;
+
     public List<Order> getAllOrder() {
         return orderRepository.findAll();
+    }
+
+    public Order createOrder(Order order) {
+        OrderCreatedEvent orderCreatedEvent = new OrderCreatedEvent(order, order.getOrderStatus());
+
+        orderProducer.sendMessage(orderCreatedEvent);
+
+        return orderRepository.save(order);
     }
 }
