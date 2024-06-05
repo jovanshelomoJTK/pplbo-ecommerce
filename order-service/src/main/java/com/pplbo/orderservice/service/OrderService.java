8a76a60a4c9b812b14599a9bdfb6bdf3a24440b8
sendist <sendisetiawan10902@gmail.com>
2024-06-05 13:46:50 +0700

    second try

--- order-service/src/main/java/com/pplbo/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/pplbo/orderservice/service/OrderService.java
@@ -7,19 +7,26 @@ import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 import com.pplbo.orderservice.event.OrderCreatedEvent;
+import com.pplbo.orderservice.kafka.KafkaProducer;
 import com.pplbo.orderservice.model.Order;
 import com.pplbo.orderservice.repository.OrderRepository;
 
 @Service
 public class OrderService {
-    private final KafkaTemplate<String, OrderCreatedEvent> kafkaTemplate;
+    // private final KafkaTemplate<String, OrderCreatedEvent> kafkaTemplate;
 
     @Autowired
     private OrderRepository orderRepository;
 
-    public OrderService(KafkaTemplate<String, OrderCreatedEvent> kafkaTemplate) {
-        this.kafkaTemplate = kafkaTemplate;
+    private KafkaProducer kafkaProducer;
+
+    public OrderService(KafkaProducer kafkaProducer) {
+        this.kafkaProducer = kafkaProducer;
     }
+
+    // public OrderService(KafkaTemplate<String, OrderCreatedEvent> kafkaTemplate) {
+    //     this.kafkaTemplate = kafkaTemplate;
+    // }
 
     public List<Order> getAllOrder() {
         return orderRepository.findAll();
@@ -27,7 +34,8 @@ public class OrderService {
 
     public Order createOrder(Order order) {
         OrderCreatedEvent orderCreatedEvent = new OrderCreatedEvent(order, order.getOrderStatus());
-        kafkaTemplate.send("order-topic", orderCreatedEvent);
+        // kafkaTemplate.send("order-topic", orderCreatedEvent);
+        kafkaProducer.sendMessage("order-topic", orderCreatedEvent.toString());
         return orderRepository.save(order);
     }
 }
