695f1f29b93aa44d6dc0511f6dc644553fa2d25c
meymeiziagaltiady <meymeizia@gmail.com>
2024-06-08 21:01:13 +0700

    fix

--- promotion-service/src/main/java/com/pplbo/promotionservice/PromotionServiceApplication.java
+++ promotion-service/src/main/java/com/pplbo/promotionservice/PromotionServiceApplication.java
@@ -4,16 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableScheduling
 @EnableAspectJAutoProxy(proxyTargetClass = true, exposeProxy = true)
 public class PromotionServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PromotionServiceApplication.class, args);
 	}
-
 }
