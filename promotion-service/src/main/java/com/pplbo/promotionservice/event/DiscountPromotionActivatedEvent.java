e46bbc04bd80e552553e5c9ee170cf2fa2d941cb
meymeiziagaltiady <meymeizia@gmail.com>
2024-06-09 14:02:54 +0700

    fix event

--- promotion-service/src/main/java/com/pplbo/promotionservice/event/DiscountPromotionActivatedEvent.java
+++ promotion-service/src/main/java/com/pplbo/promotionservice/event/DiscountPromotionActivatedEvent.java
@@ -1,13 +1,11 @@
 package com.pplbo.promotionservice.event;
 
 import com.pplbo.promotionservice.model.Promotion;
-import org.springframework.context.ApplicationEvent;
 
-public class DiscountPromotionActivatedEvent extends ApplicationEvent {
+public class DiscountPromotionActivatedEvent implements Event {
     private final Promotion promotion;
 
-    public DiscountPromotionActivatedEvent(Object source, Promotion promotion) {
-        super(source);
+    public DiscountPromotionActivatedEvent(Promotion promotion) {
         this.promotion = promotion;
     }
 
